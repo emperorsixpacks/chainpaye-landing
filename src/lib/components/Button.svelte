b632984e3bb06c8eb019984f5474cc1a6ae46632
emperorsixpacks <andrewdvd10@gmail.com>
2025-12-28 20:44:53 +0100

    feat: refactor Button component to be a link with newTab functionality

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
   import Icon from "@iconify/svelte";
 
-  const { text, url = "#", icon = "", swap = false } = $props();
+  const { text, url = "#", icon = "", swap = false, newTab = true } = $props();
 </script>
 
-<button class="bg-blue-700 text-white py-3 px-6 rounded-lg cursor-pointer">
-  <span class="flex items-center gap-2 font-bold" class:flex-row-reverse={swap}
-    ><Icon {icon} width="20" height="20" />{text}
+<a
+  href={url}
+  class="inline-block bg-blue-700 text-white py-3 px-6 rounded-lg cursor-pointer"
+  target={newTab ? "_blank" : "_self"}
+  rel={newTab ? "noopener noreferrer" : undefined}
+>
+  <span class="flex items-center gap-2 font-bold" class:flex-row-reverse={swap}>
+    {#if icon}
+      <Icon {icon} width="20" height="20" />
+    {/if}
+    {text}
   </span>
-</button>
+</a>
