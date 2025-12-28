2a6fd9af237a8d3f7f613d8b03edd9886d98bba1
emperorsixpacks <andrewdvd10@gmail.com>
2025-12-28 17:45:09 +0100

    feat: add landing page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import Hero from "$lib/components/sections/Hero.svelte";
+  import SendAndReceive from "$lib/components/sections/SendAndReceive.svelte";
+  import CashOut from "$lib/components/sections/CashOut.svelte";
+  import SpendStablecoins from "$lib/components/sections/SpendStablecoins.svelte";
+  import GetStarted from "$lib/components/sections/GetStarted.svelte";
+  import TransformingPayments from "$lib/components/sections/TransformingPayments.svelte";
+  import RealWorld from "$lib/components/sections/RealWorld.svelte";
+  import PaymentAPIs from "$lib/components/sections/PaymentAPIs.svelte";
+</script>
+
+<div class="space-y-60">
+  <div class="space-y-0 lg:space-y-80">
+    <Hero />
+    <div class="space-y-100">
+      <SendAndReceive />
+      <!-- <CashOut /> -->
+    </div>
+  </div>
+</div>
+<!-- <div class="hidden md:block">
+  <SpendStablecoins />
+</div> -->
+<GetStarted />
+<TransformingPayments />
+<RealWorld />
+<!-- <PaymentAPIs /> -->
